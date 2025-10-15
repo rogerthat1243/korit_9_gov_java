746558b92b39588fdda1f151e3d277b09b08b83b
rogerthat1243 <lragusdn0@gmail.com>
2025-10-15 17:21:34 +0900

    신규 코맛

---
+++ java_study/src/main/java/com/korit/study/ch19/최상위클래스.java
@@ -0,0 +1,35 @@
+package main.java.com.korit.study.ch19;
+
+import java.util.Scanner;
+
+public class 최상위클래스 {
+    public static void main(String[] args) {
+        int a = 19;
+        double b = a;
+        Object c = a;
+        Object d = b;
+        Object e = "test";
+        String f = "abc";
+        Car car = new Car("k3", "화이트");
+        Car car2 = new Car("k3", "화이트");
+        Object g = (Object) car;
+        String h = car.toString();
+
+        System.out.println(car);
+//        System.out.println(c + d);
+        String name1 = "김준일";
+        String name2 = "김준일";
+        boolean result1 = name1 == name2;
+        System.out.println(result1);
+        String name3 = new String("김준일");
+        System.out.println(name3 + " 입니다");
+        Scanner scanner = new Scanner(System.in);
+        String name4 = scanner.nextLine();
+        System.out.println(name4);
+        boolean result2 = name1.equals(name3);
+        System.out.println(result2);
+        boolean result3 = name2 == name4;
+        System.out.println(result3);
+
+    }
+}
